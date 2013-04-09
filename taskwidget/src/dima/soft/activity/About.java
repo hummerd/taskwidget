b0472a161e634b74f7a617cb9ce4ce5ac84e135c
Dima <dima.github@gmail.com>
2013-04-09 17:10:36 +0400

    Move to holo about activity

--- taskwidget/src/dima/soft/activity/About.java
+++ taskwidget/src/dima/soft/activity/About.java
@@ -1,40 +1,64 @@
 package dima.soft.activity;
 
-import org.holoeverywhere.preference.Preference;
-import org.holoeverywhere.preference.Preference.OnPreferenceClickListener;
-import org.holoeverywhere.preference.PreferenceActivity;
+import org.holoeverywhere.ArrayAdapter;
+import org.holoeverywhere.app.ListActivity;
 
+import android.content.Context;
 import android.os.Bundle;
-//import android.preference.Preference;
-//import android.preference.PreferenceActivity;
-//import android.preference.Preference.OnPreferenceClickListener;
-import dima.soft.R;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 
-public class About extends PreferenceActivity {
+public class About extends ListActivity {
+
+	private class AboutItem	{
+		public CharSequence mName;
+		public CharSequence mDescription;
+		
+		public AboutItem(CharSequence name, CharSequence description) {
+			mName = name;
+			mDescription = description;
+		}
+	}
+	
+	private class AboutItemAdapter extends ArrayAdapter<AboutItem> 	{
+		private AboutItem[] mObjects;
+		
+		
+		public AboutItemAdapter(Context context, AboutItem[] objects) {
+			super(context, android.R.id.text1, objects);
+			mObjects = objects;
+		}
+		
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			if (convertView == null) {
+                convertView = getLayoutInflater().inflate(org.holoeverywhere.R.layout.simple_list_item_2);
+            }
+		       
+			AboutItem obj = mObjects[position];
+			TextView textName = (TextView)convertView.findViewById(android.R.id.text1);
+			textName.setText(obj.mName);
+			
+			TextView textDesc = (TextView)convertView.findViewById(android.R.id.text2);
+			textDesc.setText(obj.mDescription);
+			return convertView;
+		}
+	}
+	
+	
 	
 	/** Called when the activity is first created. */
-	@SuppressWarnings("deprecation")
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
 	    
-	    addPreferencesFromResource(R.xml.aboutpref);
-	    initCustomPrefs();
+	    AboutItem[] items = new AboutItem[] { 
+	    		new AboutItem("Version", "Version description"), 
+	    		new AboutItem("License", "License description") };
+	    AboutItemAdapter adapter = new AboutItemAdapter(this, items);
+	    setListAdapter(adapter);
 	}
-	
-	
-	@SuppressWarnings("deprecation")
-	private void initCustomPrefs() {
-	    Preference customPref = (Preference)findPreference("prefAccount");
-	    customPref.setOnPreferenceClickListener(onShowAboutDialog);
-	
-	}
-	
-	private OnPreferenceClickListener onShowAboutDialog = new OnPreferenceClickListener() {
-		public boolean onPreferenceClick(Preference preference) {
-			
-			return false;
-	    }
-	};
 }
